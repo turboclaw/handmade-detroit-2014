6ec14e0e7107a2f7baf8a9f17980f4147370bb65
Dan Nixon <dan@turboclaw.com>
2014-05-30 22:41:19 -0400

    normalize whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 var gulp = require("gulp");
 var minifyCSS = require("gulp-minify-css");
-
 gulp.task("minify-css", function() {
-  gulp.src("styles-import.css")
-    .pipe(minifyCSS({}))
-    .pipe(gulp.dest("styles.css"))
+    gulp.src("styles-import.css")
+        .pipe(minifyCSS({}))
+        .pipe(gulp.dest("styles.css"));
 });
-
 gulp.task("default", ["minify-css"]);
