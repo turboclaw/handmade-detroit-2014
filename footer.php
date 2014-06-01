bffac917cf8b81b00ba956e5b06bb684299eabef
Dan Nixon <dan@turboclaw.com>
2014-06-01 12:08:17 -0400

    barely started the wp template conversion

---
+++ footer.php
@@ -0,0 +1,13 @@
+        <footer role="contentinfo" class="cf">
+            <small>© 2014, Handmade Detroit</small>
+            <ul class="social">
+                <li><a href="https://www.facebook.com/handmadedetroit" data-icon="facebook"><span>Facebook</span></a></li>
+                <li><a href="https://twitter.com/handmadedetroit" data-icon="twitter"><span>Twitter</span></a></li>
+                <li><a href="https://www.flickr.com/photos/hd_ducf/" data-icon="flickr"><span>Flickr</span></a></li>
+                <li><a href="https://www.etsy.com/shop/handmadedetroit" data-icon="etsy"><span>Etsy</span></a></li>
+                <li><a href="#rss" data-icon="rss"><span>RSS</span></a></li>
+            </ul>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png" />
+        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/2014-src.css" />
+        <?php wp_head(); ?>
+    </head>
+    <body class="<?php body_class(); ?>">
+        <header role="banner" class="masthead">
+            <h1 class="masthead__logo"><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/handmade-detroit.svg" alt="Handmade Detroit" /></a></h1>
+            <nav role="navigation" class="masthead__nav">
+                <ul>
+                    <li><a href="#">D.U.C.F.</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
