7e5385d3f08b4069295f9bc99b028b4f1872185f
Dan Nixon <dan@turboclaw.com>
2014-06-01 22:14:53 -0400

    fixed title tag

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <title><?php bloginfo('name'); ?><?php if ( !is_home() ) { echo " | "; wp_title(""); } ?></title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
