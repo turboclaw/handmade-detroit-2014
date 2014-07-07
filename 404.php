711f1d3b0eb084354b6ba65b0e3ee8f77aed99d5
Dan Nixon <dan@turboclaw.com>
2014-07-06 23:09:56 -0400

    404040404040404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<main role="main">
+    <article class="article article--full">
+        <header class="article__header">
+            <h1 class="article__title">Yeahhhhh, sorry about that.</h1>
+        </header>
+        <div class="article__body cf">
+            <p><img src="<?php bloginfo("template_directory") ?>/img/wat.gif" alt="WAT" style="width: 100%;" /></p>
+            <p>We've been moving some stuff around lately. Try some options from the nav up at the top of the page, or <a href="/contact/">contact us</a> if you keep ending up back here!</p>
+        </div>
+    </article>
+</main>
+<?php get_footer(); ?>
