4d2f8bd535ab39db6ad0a9055a7a6271605ff0b8
Dan Nixon <dan@turboclaw.com>
2014-07-12 15:49:20 -0400

    more circles, make the panels work on ios

--- single.php
+++ single.php
@@ -13,7 +13,9 @@
                 by <?php the_author_posts_link(); ?> on <a href="<?php the_time('/Y/m/'); ?>"><?php the_time('F'); ?></a> <?php the_time('j, Y'); ?> // Categories: <?php the_category(', '); ?><?php comments_number( '', ' // 1 Comment', ' // % Comments' ); ?> // Share on <a href="http://www.facebook.com/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>&t=<?php echo urlencode( get_the_title() ); ?>" data-icon="facebook" target="_blank"></a> <a href="http://twitter.com/share?url=<?php echo urlencode( get_permalink() ); ?>&text=<?php echo urlencode( get_the_title() ); ?>" data-icon="twitter" target="_blank"></a> <a href="http://www.pinterest.com/pin/create/button/?url=<?php echo urlencode( get_permalink() ); ?>&media=&description=<?php echo urlencode( get_the_title() ); ?>" data-icon="pinterest" target="_blank"></a>
             </div>
             <div class="article__meta__image">
-                <?php echo get_avatar( get_the_author_meta( 'ID' ), 200 ); ?>
+                <div class="article__meta__avatar">
+                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 200 ); ?>                    
+                </div>
             </div>
         </footer>
     </article>
