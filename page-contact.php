26b0f3d778dd17ee4c6f7b59152ed039c5ef311e
Dan Nixon <dan@turboclaw.com>
2014-07-15 22:21:12 -0400

    added contact custom page, new favicon syntax

---
+++ page-contact.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Page - Contact
+*/
+?>
+<?php get_header(); ?>
+<main role="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class( "article article--full" ); ?>>
+        <header class="article__header">
+            <h1 class="article__title"><?php the_title(); ?></h1>
+        </header>
+        <div class="article__body cf">
+	        <?php the_content(); ?>
+
+	        <h3>Send us an email</h3>
+	        <?php global $ddfm; echo $ddfm{1}->generate_data(); ?>
+	        <h3>Sign up for our mailing list</h3>
+	        <div class="ddfmwrap">
+	        	<form method="post" action="http://oi.vresp.com?fid=7139dcab8b" target="vr_optin_popup" onsubmit="window.open( 'http://www.verticalresponse.com', 'vr_optin_popup', 'scrollbars=yes,width=600,height=450' ); return true;" >
+	        		<p><label for="email_address">Email</label><input name="email_address" size="15" class="text" /></p>
+	        		<p><input type="submit" value="Sign me up!" style="submit" /></p>
+	        	</form>
+	        </div>
+        </div>
+    </article>
+
+    <?php endwhile; ?>
+    <?php else : ?>
+    <article id="post-not-found" class="article">
+        uh
+    </article>
+
+    <?php endif; ?>
+</main>
+<?php get_footer(); ?>
