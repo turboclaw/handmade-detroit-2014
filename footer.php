5329db78715918fec8ae3aa2bc041a3ddc1902d8
Dan Nixon <dan@turboclaw.com>
2014-07-16 22:39:21 -0400

    goog analytics

--- footer.php
+++ footer.php
@@ -8,6 +8,15 @@
                 <li><a href="http://feeds2.feedburner.com/handmadedetroit/rss" data-icon="rss"><span>RSS</span></a></li>
             </ul>
         </footer>
+        <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+        </script>
+        <script type="text/javascript">
+        try {
+        var pageTracker = _gat._getTracker("UA-7347937-1");
+        pageTracker._trackPageview();
+        } catch(err) {}</script>
         <?php wp_footer(); ?>
     </body>
 </html>
