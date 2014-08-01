17ea3c0b476617f7b0926b0cdd3e402aa1c8b0cf
Dan Nixon <dan@turboclaw.com>
2014-07-31 22:14:19 -0400

    added handheld classes to apply form

--- page-apply-form.php
+++ page-apply-form.php
@@ -21,15 +21,15 @@ Template Name: Application Form
                     <label for="contact_name">Your Name</label>
                     <input type="text" name="contact_name" id="contact_name" required />
                 </div>
-                <div class="form__control-group grid__cell--one-third">
+                <div class="form__control-group grid__cell--one-third grid__cell--handheld--one">
                     <label for="website_url">Website</label>
                     <input type="url" name="website_url" id="website_url" />
                 </div>
-                <div class="form__control-group grid__cell--one-third">
+                <div class="form__control-group grid__cell--one-third grid__cell--handheld--one">
                     <label for="email_address">Email</label>
                     <input type="email" name="email_address" id="email_address" required />
                 </div>
-                <div class="form__control-group grid__cell--one-third">
+                <div class="form__control-group grid__cell--one-third grid__cell--handheld--one">
                     <label for="phone">Phone</label>
                     <input type="tel" name="phone" id="phone" />
                 </div>
@@ -37,21 +37,21 @@ Template Name: Application Form
                     <label for="address_street">Street Address</label>
                     <input type="text" name="address_street" id="address_street" />
                 </div>
-                <div class="form__control-group grid__cell--one-third">
+                <div class="form__control-group grid__cell--one-third grid__cell--handheld--one">
                     <label for="address_city">City</label>
                     <input type="text" name="address_city" id="address_city" />
                 </div>
-                <div class="form__control-group grid__cell--one-third">
+                <div class="form__control-group grid__cell--one-third grid__cell--handheld--one">
                     <label for="address_state">State/Province</label>
                     <select name="address_state">
                         <option value="" selected="selected">--</option><option value="AL">Alabama</option><option value="AK">Alaska</option><option value="AZ">Arizona</option><option value="AR">Arkansas</option><option value="CA">California</option><option value="CO">Colorado</option><option value="CT">Connecticut</option><option value="DE">Delaware</option><option value="DC">District Of Columbia</option><option value="FL">Florida</option><option value="GA">Georgia</option><option value="HI">Hawaii</option><option value="ID">Idaho</option><option value="IL">Illinois</option><option value="IN">Indiana</option><option value="IA">Iowa</option><option value="KS">Kansas</option><option value="KY">Kentucky</option><option value="LA">Louisiana</option><option value="ME">Maine</option><option value="MD">Maryland</option><option value="MA">Massachusetts</option><option value="MI">Michigan</option><option value="MN">Minnesota</option><option value="MS">Mississippi</option><option value="MO">Missouri</option><option value="MT">Montana</option><option value="NE">Nebraska</option><option value="NV">Nevada</option><option value="NH">New Hampshire</option><option value="NJ">New Jersey</option><option value="NM">New Mexico</option><option value="NY">New York</option><option value="NC">North Carolina</option><option value="ND">North Dakota</option><option value="OH">Ohio</option><option value="OK">Oklahoma</option><option value="OR">Oregon</option><option value="PA">Pennsylvania</option><option value="RI">Rhode Island</option><option value="SC">South Carolina</option><option value="SD">South Dakota</option><option value="TN">Tennessee</option><option value="TX">Texas</option><option value="UT">Utah</option><option value="VT">Vermont</option><option value="VA">Virginia</option><option value="WA">Washington</option><option value="WV">West Virginia</option><option value="WI">Wisconsin</option><option value="WY">Wyoming</option><option value"">------</option><option value="AB">Alberta</option><option value="BC">British Columbia</option><option value="MB">Manitoba</option><option value="NB">New Brunswick</option><option value="NL">Newfoundland</option><option value="NT">NW Territories</option><option value="NS">Nova Scotia</option><option value="NU">Nunavut</option><option value="ON">Ontario</option><option value="PE">Prince Edward Island</option><option value="QC">Quebec</option><option value="SK">Saskatchewan</option><option value="YT">Yukon</option>
                     </select>
                 </div>
-                <div class="form__control-group grid__cell--one-third">
+                <div class="form__control-group grid__cell--one-third grid__cell--handheld--one">
                     <label for="address_zip" class="short">Postal Code</label>
                     <input type="text" name="address_zip" id="address_zip" />
                 </div>
-                <div class="form__control-group grid__cell--one-half">
+                <div class="form__control-group grid__cell--one-half grid__cell--handheld--one">
                     <label for="craft_id">What type of crafts do you primarily sell?</label>
                     <select name="craft_id" id="craft_id">
                         <option value="" selected="selected">Select One</option>
@@ -72,7 +72,7 @@ Template Name: Application Form
                     </select>
                     <label><small>If you wish to specify multiple categories, let us know in the comments below.</small></label>
                 </div>
-                <div class="form__control-group grid__cell--one-half">
+                <div class="form__control-group grid__cell--one-half grid__cell--handheld--one">
                     <label for="pricerange">What is the price range of your items?</label>
                     <input type="text" name="pricerange" id="pricerange" />
                 </div>
@@ -88,14 +88,14 @@ Template Name: Application Form
                     <label for="photos_0">The Detroit Urban Craft Fair is a juried event. Please submit up to five images of the work you plan to sell.</label>
                     <label class="note">The filesize of each photo <em>must</em> be less than 500kb. Your application may not process successfully if your images are too large.</label>
                     <div id="photo_uploads" class="grid">
-                        <div class="grid__cell--one-third"><input type="file" name="photos_0" id="photos_0" /></div>
-                        <div class="grid__cell--one-third"><input type="file" name="photos_1" id="photos_1" /></div>
-                        <div class="grid__cell--one-third"><input type="file" name="photos_2" id="photos_2" /></div>
-                        <div class="grid__cell--one-third"><input type="file" name="photos_3" id="photos_3" /></div>
-                        <div class="grid__cell--one-third"><input type="file" name="photos_4" id="photos_4" /></div>
+                        <div class="grid__cell--one-third grid__cell--handheld--one"><input type="file" name="photos_0" id="photos_0" /></div>
+                        <div class="grid__cell--one-third grid__cell--handheld--one"><input type="file" name="photos_1" id="photos_1" /></div>
+                        <div class="grid__cell--one-third grid__cell--handheld--one"><input type="file" name="photos_2" id="photos_2" /></div>
+                        <div class="grid__cell--one-third grid__cell--handheld--one"><input type="file" name="photos_3" id="photos_3" /></div>
+                        <div class="grid__cell--one-third grid__cell--handheld--one"><input type="file" name="photos_4" id="photos_4" /></div>
                     </div>
                 </div>
-                <div class="form__control-group grid__cell--one-half">
+                <div class="form__control-group grid__cell--one-half grid__cell--handheld--one">
                     <label for="speak_press">Are you willing to speak to the press in regards to the promotion of the Detroit Urban Craft Fair?</label>
                     <select name="speak_press" id="speak_press">
                         <option value="No">No</option>
